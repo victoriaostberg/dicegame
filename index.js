56d9dfd6382c5e49c08b67e410896a306aa926fd
victoriaostberg <150444992+victoriaostberg@users.noreply.github.com>
2023-12-15 11:54:03 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+function diceRoll(randomNumber1) {
+    console.log(randomNumber1);
+
+    if (randomNumber1 === 1) {
+        document.querySelector(".img1").setAttribute("src", "./images/dice1.png");
+    } else if (randomNumber1 === 2) {
+        document.querySelector(".img1").setAttribute("src", "./images/dice2.png");
+    } else if (randomNumber1 === 3) {
+        document.querySelector(".img1").setAttribute("src", "./images/dice3.png");
+    } else if (randomNumber1 === 4) {
+        document.querySelector(".img1").setAttribute("src", "./images/dice4.png");
+    } else if (randomNumber1 === 5) {
+        document.querySelector(".img1").setAttribute("src", "./images/dice5.png");
+    } else if (randomNumber1 === 6) {
+        document.querySelector(".img1").setAttribute("src", "./images/dice6.png");
+    }
+}
